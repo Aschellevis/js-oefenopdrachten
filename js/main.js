f20216d90ca210b408c00dd1042924b5b6dec032
Aschellevis <302953884@student.rocmondriaan.nl>
2022-06-09 09:58:38 +0200

    Opdracht 2 af

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-//Opdracht 1
+const button = document.querySelector('.myButton');
+const names = document.querySelector('.name');
 
-let myName = prompt('Wat is uw naam?', );
+let myName = 'anouska schellevis';
 console.log(myName.toUpperCase( ));
+
+let hisName = 'jan jansen';
+console.log(hisName.toLowerCase( ));
+
+
+function myFunction () {
+    names.innerHTML += myName;
+}
+
+button.addEventListener('click', myFunction);
